35511e6183d9ec7b6193d3000162d80ce23a1599
Krish2882005 <ganatrakrish2882005@gmail.com>
2021-03-04 12:02:04 +0530

    Update Gui.cpp

--- Gui.cpp
+++ Gui.cpp
@@ -69,28 +69,28 @@ std::array<int, 2> Gui::InputHandling()
 {
 	switch (Init::Event.type)
 	{
-	case SDL_MOUSEMOTION:
-	{
-		MouseX = Init::Event.motion.x;
-		MouseY = Init::Event.motion.y;
-		return { 0, 0 };
-		break;
-	}
-	case SDL_MOUSEBUTTONDOWN:
-	{
-		if (Init::Event.button.button == 1)
+		case SDL_MOUSEMOTION:
 		{
 			MouseX = Init::Event.motion.x;
 			MouseY = Init::Event.motion.y;
-			return { MouseX , MouseY };
+			return { 0, 0 };
+			break;
 		}
-		break;
-	}
-	default:
-	{
-		return { 0, 0 };
-		break;
-	}
+		case SDL_MOUSEBUTTONDOWN:
+		{
+			if (Init::Event.button.button == 1)
+			{
+				MouseX = Init::Event.motion.x;
+				MouseY = Init::Event.motion.y;
+				return { MouseX , MouseY };
+			}
+			break;
+		}
+		default:
+		{
+			return { 0, 0 };
+			break;
+		}
 	}
 }
 
